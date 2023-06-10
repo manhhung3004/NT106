1ba864ff5cf9c7dd9798c009aa6081853f2f445d
manhhung20033 <21522122@gm.uit.edu.vn>
2023-06-11 03:29:42 +0700

    Fix UI/UX

--- Client/Form_Client.Designer.cs
+++ Client/Form_Client.Designer.cs
@@ -69,10 +69,10 @@
             this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(214)))), ((int)(((byte)(185)))));
             this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(214)))), ((int)(((byte)(185)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(687, 25);
+            this.button1.Location = new System.Drawing.Point(687, 13);
             this.button1.Margin = new System.Windows.Forms.Padding(0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(92, 62);
+            this.button1.Size = new System.Drawing.Size(83, 62);
             this.button1.TabIndex = 1;
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
